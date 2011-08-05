48544ef8ddc0710e972102dbfc2add90ff0d0599
Ashray Kulkarni <ashrayk@codeaurora.org>
2011-08-04 18:53:22 -0700

    mm-core: Add new roles for video-decoder.
    Load 7x30, 8660, and 8960 Video decoder with divx4 and
    divx5/6 roles.
    
    Change-Id: I57da6525af466515ae81a94470000bd5599461d4
    CRs-fixed: 297558

--- omxcore/src/8960/qc_registry_table.c
+++ omxcore/src/8960/qc_registry_table.c
@@ -104,6 +104,22 @@ omx_core_cb_type core[] =
     }
   },
   {
+    "OMX.qcom.video.decoder.divx4",
+    NULL,   // Create instance function
+    // Unique instance handle
+    {
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    },
+    NULL,   // Shared object library handle
+    "libOmxVdec.so",
+    {
+      "video_decoder.divx"
+    }
+  },
+  {
     "OMX.qcom.video.decoder.divx",
     NULL,   // Create instance function
     // Unique instance handle
